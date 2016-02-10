1434504f009deafff773dabf4af79034ec65fe84
Cassio Cardoso <caugusto.cardoso@gmail.com>
2016-02-10 11:21:35 -0200

    Add Super Bowl 46 info.

--- js/app.js
+++ js/app.js
@@ -25,8 +25,20 @@ app.service('sbwService', function($http, $q) {
 
   promise.then(function(data) {
     $scope.superbowl = data.data;
-    $scope.year = '2013';
+    $scope.year = $scope.superbowl[0].year;
   });
 
+  $scope.changeWinner = function() {
+    var yearInput = document.getElementsByName('year')[0];
+    var currentValue = yearInput.value;
 
+    if (currentValue.length < 4) {
+      // prevent change if length of value is smaller than 4
+      return;
+    } else {
+      // change current year value
+      $scope.year = currentValue;
+      yearInput.focus();
+    }
+  };
 });
